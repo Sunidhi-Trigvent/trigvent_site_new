199f578db1e880d79d3af5e3fea3b39f22c82a83
Sunidhi <sunidhi@trigvent.com>
2024-11-11 13:15:13 +0530

    Header_inprogress

--- src/view/layout/header/index.tsx
+++ src/view/layout/header/index.tsx
@@ -1,7 +1,19 @@
+import logoImage from "../../../assets/images/logo1.png"; // Import the image
+
 const Header = () => {
   return (
     <div>
-      <p>Header</p>
+      <section className="h-screen bg-blue-300">
+        <header className="bg-slate-300">
+          <nav className="flex justify-between items-center">
+            <img
+              src={logoImage} // Use the imported image here
+              alt="explore image"
+              className="w-16 h-16" // Tailwind class for width and height
+            />
+          </nav>
+        </header>
+      </section>
     </div>
   );
 };
--- src/view/layout/index.tsx
+++ src/view/layout/index.tsx
@@ -5,7 +5,6 @@ import { Outlet } from "react-router-dom";
 const Index = () => {
   return (
     <>
-      console.log("hi");
       <Header />
       <Outlet />
       <Footer />
