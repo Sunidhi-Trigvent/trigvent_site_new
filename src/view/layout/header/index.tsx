ecb673f5fd91479b3697187b0754aff2e5dd7283
Sunidhi <sunidhi@trigvent.com>
2024-11-11 16:17:22 +0530

    Header

--- src/view/layout/header/index.tsx
+++ src/view/layout/header/index.tsx
@@ -2,8 +2,11 @@ import logoImage from "../../../assets/images/logo1.png"; // Import the image
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faX } from "@fortawesome/free-solid-svg-icons"; // Correct import for faBars and faX
+import { useState } from "react"; // Import useState for managing the toggle state
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for tracking the menu status (open/close)
+
   const navItems = [
     {
       id: 1,
@@ -33,8 +36,7 @@ function Header() {
   ];
 
   const onToggleMenu = () => {
-    console.log("Menu toggled!");
-    // You can implement your toggle logic here
+    setIsMenuOpen(!isMenuOpen); // Toggle menu state
   };
 
   return (
@@ -50,9 +52,22 @@ function Header() {
               />
             </div>
 
-            {/* Header List-items */}
-            <div>
-              <div className="flex xs:flex-col lg:flex-row space-x-4">
+            {/* Menu Toggle */}
+            <div className="block lg:hidden">
+              <FontAwesomeIcon
+                onClick={onToggleMenu} // Toggle menu state
+                icon={isMenuOpen ? faX : faBars} // Toggle between faX and faBars
+                className="cursor-pointer" // Adds a pointer cursor for interactivity
+              />
+            </div>
+
+            {/* Navigation Items - Visible above lg and controlled by toggle below lg */}
+            <div
+              className={`${
+                isMenuOpen ? "block" : "hidden"
+              } lg:flex lg:space-x-4 lg:static lg:w-auto absolute top-16 left-0 w-full bg-white px-4 py-2 md:min-h-[60vh] space-y-4 lg:space-y-0`}
+            >
+              <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0">
                 {navItems.map((item) => (
                   <Link
                     key={item.id}
@@ -63,12 +78,6 @@ function Header() {
                   </Link>
                 ))}
               </div>
-            </div>
-
-            <div className="block lg:hidden">
-              {/* Correct usage of FontAwesomeIcon for both faBars and faX */}
-              <FontAwesomeIcon onClick={onToggleMenu} icon={faBars} />
-              <FontAwesomeIcon icon={faX} /> {/* Using faX close icon */}
             </div>
           </nav>
         </header>
