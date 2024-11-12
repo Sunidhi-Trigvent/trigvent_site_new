23cdbde79cd6db023646fe058244913b731dc8fc
Sunidhi <sunidhi@trigvent.com>
2024-11-12 12:13:30 +0530

    intro

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         lg: "1024px",
         xl: "1280px",
         "2xl": "1536px", // Default Tailwind '2xl'
+        "3xl": "1920px", // Custom 3xl breakpoint for large monitors
+        "4xl": "2560px", // Custom 4xl breakpoint for large desktop screens
+        "5xl": "3840px", // Custom 5xl breakpoint for ultra-wide monitors
       },
     },
   },
